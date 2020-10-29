c8d1763705b1882c65a748ab48d36bb9717aec04
Demo User <ubuntu@arm.localdomain>
2020-10-29 00:15:31 +0000

    comente

--- src/td3_myi2c.c
+++ src/td3_myi2c.c
@@ -4,13 +4,28 @@
 #include <linux/cdev.h>
 #include <linux/fs.h>
 
-#define MENOR 0
-#define CANT_DISP 1
-#define DEVICE_NAME "td3_myi2c"
+#define MENOR          0
+#define CANT_DISP      1
+#define DEVICE_NAME    "td3_myi2c"
+#define CLASS_NAME     "i2c_class"
 
-static dev_t myi2c;
-struct cdev * myi2c_cdev;
+static struct {
+	dev_t myi2c;
+
+	struct cdev * myi2c_cdev;
+	struct device * myi2c_device;
+	struct class * myi2c_class;
+} state;
+
 struct file_operations i2c_ops;
+/* = {
+	.owner = THIS_MODULE,
+	.open = myopen,
+	.read = myread,
+	.write = mywrite,
+	.release = myrelease,
+	.ioctl = myioctl,
+};*/
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Nicolas Mahnic R5054");
@@ -18,31 +33,41 @@ MODULE_VERSION("1.0");
 MODULE_DESCRIPTION("TD3_MYI2C LKM");
 
 static int __init i2c_init(void){
-	pr_alert("%s: Soy el modulo td3_myi2c del kernel\n", DEVICE_NAME);
-	myi2c_cdev = cdev_alloc();
+	int status = 0;
 	
-	if((alloc_chrdev_region(&myi2c, MENOR, CANT_DISP, DEVICE_NAME)) < 0){
+	pr_alert("%s: Soy el modulo td3_myi2c del kernel\n", DEVICE_NAME);
+	state.myi2c_cdev = cdev_alloc();
+	
+	// alloc_chrdev_region - register a range of char device numbers - https://manned.org/alloc_chrdev_region.9
+	if((status = alloc_chrdev_region(&state.myi2c, MENOR, CANT_DISP, DEVICE_NAME)) < 0){
 		pr_alert("%s: No es posible asignar el numero mayor\n", DEVICE_NAME);
-		return -1;
+		return status;
 	}
 
-	pr_alert("%s: Numero mayor asignado %d 0x%X\n", DEVICE_NAME, MAJOR(myi2c), MAJOR(myi2c));
-	//cdev_init(myi2c_cdev, &i2c_ops);
-	myi2c_cdev->ops = &i2c_ops;
-	myi2c_cdev->owner = THIS_MODULE;
-	myi2c_cdev->dev = myi2c;
-	if((cdev_add(myi2c_cdev, myi2c, CANT_DISP)) < 0){
-		unregister_chrdev_region(myi2c, CANT_DISP);
+	pr_alert("%s: Numero mayor asignado %d 0x%X\n", DEVICE_NAME, MAJOR(state.myi2c), MAJOR(state.myi2c));
+	
+	// cdev_init - initialize a cdev structure - https://manned.org/cdev_init.9
+	// Equivalent to:  state.myi2c_cdev->ops = &i2c_ops;
+	cdev_init(state.myi2c_cdev, &i2c_ops); 
+	
+	// cdev_add - add a char device to the system - https://manned.org/cdev_add.9
+	// Equivalent to: state.myi2c_cdev->owner = THIS_MODULE;
+	// 				  state.myi2c_cdev->dev = state.myi2c;
+	if((status = cdev_add(state.myi2c_cdev, state.myi2c, CANT_DISP)) < 0){
+		// unregister_chrdev_region - unregister a range of device numbers - https://manned.org/unregister_chrdev_region.9
+		unregister_chrdev_region(state.myi2c, CANT_DISP);
 		pr_alert("%s: No es no es posible registrar el dispositivo\n", DEVICE_NAME);
-		return -1;
+		return status;
 	}
 	return 0;
 }
 
 static void __exit i2c_exit(void){
 	printk(KERN_ALERT "%s: Adios mundo!. Soy el kernel\n", DEVICE_NAME);
-	cdev_del(myi2c_cdev);
-	unregister_chrdev_region(myi2c, CANT_DISP);
+	// cdev_del - remove a cdev from the system - https://manned.org/cdev_del.9
+	cdev_del(state.myi2c_cdev);
+	// unregister_chrdev_region - unregister a range of device numbers - https://manned.org/unregister_chrdev_region.9
+	unregister_chrdev_region(state.myi2c, CANT_DISP);
 }
 
 module_init(i2c_init);
